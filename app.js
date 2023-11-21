1791145a8db8023bce140082ae24816ab10dd324
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-11-21 09:44:39 +0200

    Books displayed in a list

--- app.js
+++ app.js
@@ -10,19 +10,50 @@ const haveRead = document.getElementById(`have-read`)
 
 const bookVariable = [bookTitle, bookAuthor, bookPages, bookDate, haveRead]
 
+
+//Clears form after closing or adding
 function clearForm(array){
     haveRead.checked = false
     array.map(variable => variable.value = ``)
 }
 
-function checkForEmptyBooks(){
+const titleOfBookList = document.querySelector(`.title-list`)
+const authorOfBookList = document.querySelector(`.author-list`)
+const pagesOfBookList = document.querySelector(`.pages-list`)
+const releaseOfBookList = document.querySelector(`.release-list`)
+const readOfBookList = document.querySelector(`.read-list`)
 
+function displayBooks(array){
+    const titleOfBookDisplay = document.createElement(`div`);
+    titleOfBookDisplay.classList.add(`title-of-book-display`);
+    titleOfBookList.appendChild(titleOfBookDisplay);
+
+    const authorOfBookDisplay = document.createElement(`div`);
+    authorOfBookDisplay.classList.add(`author-of-book-display`);
+    authorOfBookList.appendChild(authorOfBookDisplay)
+
+    const pagesOfBookDisplay = document.createElement(`div`);
+    pagesOfBookDisplay.classList.add(`pages-of-book-display`);
+    pagesOfBookList.appendChild(pagesOfBookDisplay)
+
+    const releaseOfBookDisplay = document.createElement(`div`)
+    releaseOfBookDisplay.classList.add(`release-of-book-display`)
+    releaseOfBookList.appendChild(releaseOfBookDisplay)
+
+    const readOfBookDisplay = document.createElement(`div`);
+    readOfBookDisplay.classList.add(`read-of-book-display`)
+    readOfBookList.appendChild(readOfBookDisplay)
+
+    array.map(book =>
+        titleOfBookDisplay.innerHTML = book.title,
+        authorOfBookDisplay.textContent = book.author,
+        pagesOfBookDisplay.textContent = book.pages,
+        releaseOfBookDisplay.textContent = book.date,
+        book.read == true ? readOfBookDisplay.style.cssText = "background-color: green;" : readOfBookDisplay.style.cssText = "background-color: red;"
+        )
 }
 
 
-function checkRequiredField(){
-
-}
 
 function Book(title, author, pages, date, read){
     this.title = title,
@@ -41,26 +72,33 @@ openForm.addEventListener(`click`, () => {
 })
 
 
-const resetForm = document.querySelector(`.rest-book-form`);
-
 
 const addBookButton = document.querySelector(`.add-book-button`);
 addBookButton.addEventListener(`click`, () => {
     console.log(`submit works`)
-    book = new Book(
-        bookTitle.value,
-        bookAuthor.value,
-        bookPages.value,
-        bookDate.value,
-        haveRead.checked
-    )
-    bookLibrary.push(book)
-    // bookForm.style.display = `none`
-    // clearForm(bookVariable)
+    if(!bookTitle.value == ``){
+        book = new Book(
+            bookTitle.value,
+            bookAuthor.value,
+            bookPages.value,
+            bookDate.value,
+            haveRead.checked
+        )
+        bookLibrary.push(book)
+        displayBooks(bookLibrary)
+    }
+    if(!bookTitle.value == ``){
+        bookForm.style.display = `none`
+        clearForm(bookVariable)
+    }
     console.log(bookLibrary)
 });
 
-
+const closeForm = document.querySelector(`.icon-close`);
+closeForm.addEventListener(`click`, ()=>{
+    bookForm.style.display = `none`;
+    clearForm(bookVariable)
+})
 
 
 
