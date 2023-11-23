70d92f4d50c7d4e144b9a23e9c7ecbbe1ca54e0d
Edvinas-Ralys <edvinas.ralys97@gmail.com>
2023-11-23 16:17:53 +0200

    Added button to remove books

--- app.js
+++ app.js
@@ -6,121 +6,112 @@ const bookTitle = document.getElementById(`book-title`);
 const bookAuthor = document.getElementById(`book-author`);
 const bookPages = document.getElementById(`book-pages`);
 const bookDate = document.getElementById(`book-date`);
-const haveRead = document.getElementById(`have-read`)
+const haveRead = document.getElementById(`have-read`);
 
-const bookVariable = [bookTitle, bookAuthor, bookPages, bookDate, haveRead]
-
+const bookVariable = [bookTitle, bookAuthor, bookPages, bookDate, haveRead];
 
 //Clears form after closing or adding book
-function clearForm(array){
-    haveRead.checked = false
-    array.map(variable => variable.value = ``)
+function clearForm(array) {
+  haveRead.checked = false;
+  array.map((variable) => (variable.value = ``));
 }
-const bookArea = document.querySelector(`.book-content`)
+const bookArea = document.querySelector(`.book-content`);
 
-const titleOfBookList = document.querySelector(`.title-list`)
-const authorOfBookList = document.querySelector(`.author-list`)
-const pagesOfBookList = document.querySelector(`.pages-list`)
-const releaseOfBookList = document.querySelector(`.release-list`)
-const readOfBookList = document.querySelector(`.read-list`)
+const titleOfBookList = document.querySelector(`.title-list`);
+const authorOfBookList = document.querySelector(`.author-list`);
+const pagesOfBookList = document.querySelector(`.pages-list`);
+const releaseOfBookList = document.querySelector(`.release-list`);
+const readOfBookList = document.querySelector(`.read-list`);
 
-function displayBooks(array){
-    const bookElemet = document.createElement(`div`)
-    bookElemet.classList.add(`book-number${bookLibrary.length}`)
-    bookArea.appendChild(bookElemet)
+function displayBooks(array) {
+  const bookElemet = document.createElement(`div`);
+  bookElemet.classList.add(`book-number${bookLibrary.length}`);
+  bookArea.appendChild(bookElemet);
 
-    const titleOfBookDisplay = document.createElement(`div`);
-    titleOfBookDisplay.classList.add(`title-of-book-display`);
-    bookElemet.appendChild(titleOfBookDisplay);
+  const titleOfBookDisplay = document.createElement(`div`);
+  titleOfBookDisplay.classList.add(`title-of-book-display`);
+  bookElemet.appendChild(titleOfBookDisplay);
 
-    const authorOfBookDisplay = document.createElement(`div`);
-    authorOfBookDisplay.classList.add(`author-of-book-display`);
-    bookElemet.appendChild(authorOfBookDisplay)
+  const authorOfBookDisplay = document.createElement(`div`);
+  authorOfBookDisplay.classList.add(`author-of-book-display`);
+  bookElemet.appendChild(authorOfBookDisplay);
 
-    const pagesOfBookDisplay = document.createElement(`div`);
-    pagesOfBookDisplay.classList.add(`pages-of-book-display`);
-    bookElemet.appendChild(pagesOfBookDisplay)
+  const pagesOfBookDisplay = document.createElement(`div`);
+  pagesOfBookDisplay.classList.add(`pages-of-book-display`);
+  bookElemet.appendChild(pagesOfBookDisplay);
 
-    const releaseOfBookDisplay = document.createElement(`div`)
-    releaseOfBookDisplay.classList.add(`release-of-book-display`)
-    bookElemet.appendChild(releaseOfBookDisplay)
+  const releaseOfBookDisplay = document.createElement(`div`);
+  releaseOfBookDisplay.classList.add(`release-of-book-display`);
+  bookElemet.appendChild(releaseOfBookDisplay);
 
+  const readOfBookDisplay = document.createElement(`div`);
+  readOfBookDisplay.classList.add(`read-of-book-display`);
+  readOfBookDisplay.addEventListener(`click`, () => {
+    if (readOfBookDisplay.style.cssText == "background-color: green;") {
+      readOfBookDisplay.style.cssText = "background-color: red;";
+    } else if (readOfBookDisplay.style.cssText == "background-color: red;") {
+      readOfBookDisplay.style.cssText = "background-color: green;";
+    }
+  });
+  bookElemet.appendChild(readOfBookDisplay);
 
+  const removeBook = document.createElement(`button`);
+  removeBook.classList.add(`remove-button${bookLibrary.length}`);
+  removeBook.textContent = `Remove`;
+  bookElemet.appendChild(removeBook);
+  removeBook.addEventListener(`click`, () => {
+    bookElemet.remove();
+  });
 
-    const readOfBookDisplay = document.createElement(`div`);
-    readOfBookDisplay.classList.add(`read-of-book-display`);
-    readOfBookDisplay.addEventListener(`click`, ()=>{
-        if(readOfBookDisplay.style.cssText == "background-color: green;"){
-            readOfBookDisplay.style.cssText = "background-color: red;"
-        }
-        else if(readOfBookDisplay.style.cssText == "background-color: red;"){
-            readOfBookDisplay.style.cssText = "background-color: green;"
-        }
-    })
-    bookElemet.appendChild(readOfBookDisplay)
-
-    const removeBook = document.createElement(`button`)
-    removeBook.classList.add(`remove-button${bookLibrary.length}`)
-    removeBook.textContent = `Remove`
-    bookElemet.appendChild(removeBook)
-
-    array.map(book =>
-        titleOfBookDisplay.textContent = book.title,
-        authorOfBookDisplay.textContent = book.author,
-        pagesOfBookDisplay.textContent = book.pages,
-        releaseOfBookDisplay.textContent = book.date,
-        book.read == true ? readOfBookDisplay.style.cssText = "background-color: green;" : readOfBookDisplay.style.cssText = "background-color: red;"
-        )
+  array.map(
+    (book) => (titleOfBookDisplay.textContent = book.title),
+    (authorOfBookDisplay.textContent = book.author),
+    (pagesOfBookDisplay.textContent = book.pages),
+    (releaseOfBookDisplay.textContent = book.date),
+    book.read == true
+      ? (readOfBookDisplay.style.cssText = "background-color: green;")
+      : (readOfBookDisplay.style.cssText = "background-color: red;")
+  );
 }
 
+function Book(title, author, pages, date, read) {
+  (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.date = date),
+    (this.read = read);
+}
 
-
-function Book(title, author, pages, date, read){
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.date = date,
-    this.read = read;
-};
-
-const bookForm = document.querySelector(`.book-form`)
+const bookForm = document.querySelector(`.book-form`);
 
 const openForm = document.querySelector(`.add-book`);
 openForm.addEventListener(`click`, () => {
-    bookForm.style.display = `flex`;
-    console.log(`works open form`)
-})
-
-
+  bookForm.style.display = `flex`;
+  console.log(`works open form`);
+});
 
 const addBookButton = document.querySelector(`.add-book-button`);
 addBookButton.addEventListener(`click`, () => {
-    console.log(`submit works`)
-    if(!bookTitle.value == ``){
-        book = new Book(
-            bookTitle.value,
-            bookAuthor.value,
-            bookPages.value,
-            bookDate.value,
-            haveRead.checked
-        )
-        bookLibrary.push(book)
-        displayBooks(bookLibrary)
-    }
-    if(!bookTitle.value == ``){
-        bookForm.style.display = `none`
-        clearForm(bookVariable)
-    }
+  console.log(`submit works`);
+  if (!bookTitle.value == ``) {
+    book = new Book(
+      bookTitle.value,
+      bookAuthor.value,
+      bookPages.value,
+      bookDate.value,
+      haveRead.checked
+    );
+    bookLibrary.push(book);
+    displayBooks(bookLibrary);
+  }
+  if (!bookTitle.value == ``) {
+    bookForm.style.display = `none`;
+    clearForm(bookVariable);
+  }
 });
 
 const closeForm = document.querySelector(`.icon-close`);
-closeForm.addEventListener(`click`, ()=>{
-    bookForm.style.display = `none`;
-    clearForm(bookVariable)
-})
-
-
-
-
-
-
+closeForm.addEventListener(`click`, () => {
+  bookForm.style.display = `none`;
+  clearForm(bookVariable);
+});
